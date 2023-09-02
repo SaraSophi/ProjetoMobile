3bfe7e5fe2fed03a9705eb1ce4910954dd83d0c1
SaraSophi <117236731+SaraSophi@users.noreply.github.com>
2023-09-01 21:46:05 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -20,47 +20,77 @@
 // });
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 const App = () => {
 
     const [nome, setNome] = useState('');
     const [email, setEmail]= useState('');
     const [senha, setSenha]= useState('');
-
-    const onChangeNome = (value)=>{
-
-      if(value.length<=2){
-        console.log('Incorreto')
-        
-      }else{
-        console.log('Correto')      }
-      setNome(value)
+    const [errorNome, setErrorNome] = useState(null);
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorSenha, setErrorSenha] = useState(null);
+  
+    // const onChangeNome = (value)=>{
+    //   if(value.length<=2){
+    //     console.log('Incorreto')   
+    //   }else{
+    //     console.log('Correto')      }
+    //   setNome(value)
+    const cadastro = () =>{
+      if(email ===""){
+        setErrorEmail("E-mail inválido, tente novamente!")
+        // error = true
+        alert("Ok")
+      salvar()
+      }
+      
     }
+    const salvar= ()=>{
+      if(validacao()){
+        console.log("Salvouu")
+      }
+    }
+    const validacao= ()=>{
+      // let error = false
+      
+      // return !error
+    }
+  
+
+    
     return (
         <View style={styles.container}>
     <StatusBar hidden/>
     <TextInput 
     placeholder='Seu Nome...' 
     style={styles.textInput} 
-    onChangeText={onChangeNome}
-    value={nome}
+    onChangeText={text=>{setNome (text), setErrorNome(null)}}
+    errorMensage={errorNome}
     />
     
 
-    <TextInput placeholder='Seu e-mail...' 
+    <TextInput placeholder='name@gmail.com' 
     style={styles.textInput} 
-    onChangeText={text=>setEmail (text)}
+    onChangeText={text=>{setEmail (text), setErrorEmail(null)} }
+    errorMensage={errorEmail}
+
     />
     <TextInput secureTextEntry={true} 
     placeholder='Sua Senha...' 
     style={styles.textInput}
     onChangeText={text=>setSenha (text)}
+    errorMensage={errorSenha}
+
     />
-        
+          {email === 'true' ? <Text>E-mail inválido, tente novamente!</Text> : <></> }
+
+        <TouchableOpacity style={styles.button} onPress={cadastro}>
+          <Text>Cadastrar</Text>
+        </TouchableOpacity>
     </View>
     );
-   
-}
+    
+    }
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -76,7 +106,15 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         paddingLeft: 20,
         borderRadius: 20,
-        marginBottom: 10,
+        marginBottom: 26,
+
+    },
+    button:{
+      backgroundColor: 'yellow',
+      padding: 15,
+      marginTop: 20,
+      borderRadius: 20,
+      justifyContent: 'center',
 
     }
 })
